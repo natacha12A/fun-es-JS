0e6051a1b9fcd82ffaa6e021671c1bdf3403e4e5
Natacha Tavares <natacha.alves@eaportal.org>
2025-04-25 21:53:47 -0300

    Exemplo 4, JavaScript

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function somar (numero1, numero2) {
     somar (19, 289)
 */
 
+/* ## Exemplo 3
     function raizQuadrada (numero) {
     console.log(Math.sqrt(numero))
     }
@@ -35,3 +36,24 @@ function somar (numero1, numero2) {
     raizQuadrada (numero1)
     potenciacao (numero1, numero2)
     multiplicacao (numero2, numero1)
+*/
+
+function raizQuadrada(numero){
+    return Math.sqrt(numero)
+}
+
+function potenciacao(valor1, valor2) {
+    return Math.pow(valor1, valor2)
+}
+
+function multiplicacao (valor1, valor2) {
+    return valor1 * valor2
+}
+
+let numero1 = Number(prompt("Digite o número 1:"))
+let numero2 = Number(prompt("Digite o número 2:"))
+
+let raiz1 = raizQuadrada (numero1)
+let raiz2 = raizQuadrada (numero2)
+
+console.log(raiz1, raiz2)
