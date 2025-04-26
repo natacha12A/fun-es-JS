3f041a6ae18fb12c5b79ce39c577369b14c12588
Natacha Tavares <natacha.alves@eaportal.org>
2025-04-25 21:37:59 -0300

    Exemplo 2, JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*## exemplo 1
 
 function mensagem() {
 alert("Seja bem-vindo(a)!")
@@ -6,5 +6,13 @@ console.log("Seja bem-vindo(a)!")
 console.log("Você é muito legal!")
 }
 mensagem()
+*/
 
-
+function somar (numero1, numero2) {
+    console.log(numero1 + numero2)
+    }
+    let numero1 = 40
+    let numero2 = 23
+    somar (numero1, numero2)
+    somar (30, 12)
+    somar (19, 289)
